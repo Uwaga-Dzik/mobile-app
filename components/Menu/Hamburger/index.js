4c3cf2d5415150eb0854d9ef4b4acbd883e32de7
Kbuczynski <kamil.buczynski00@interia.pl>
2020-11-27 19:11:24 +0100

    create header

--- components/Menu/Hamburger/index.js
+++ components/Menu/Hamburger/index.js
@@ -1,30 +1,51 @@
+import PropTypes from "prop-types";
 import React from "react";
 import styled from "styled-components";
-import { Text } from "react-native";
 
 const StyledHamburger = styled.TouchableHighlight`
   position: relative;
-  width: 30px;
-  height: 3px;
-  background-color: ${({ theme }) => theme.colors.black};
-
-  ::before {
-    content: "";
-    position: absolute;
-    top: 2px;
-    left: 0;
-    width: 30px;
-    height: 3px;
-    background-color: ${({ theme }) => theme.colors.orange};
-  }
+  height: 25px;
+  width: 40px;
+  padding: 0 0;
 `;
 
-const Hamburger = () => {
+const StyledHamburgerTop = styled.View`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 40px;
+  height: 5px;
+  background-color: ${({ theme }) => theme.colors.black};
+  border-radius: 18px;
+`;
+
+const StyledHamburgerBottom = styled.View`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 40px;
+  height: 5px;
+  background-color: ${({ theme }) => theme.colors.black};
+  border-radius: 18px;
+`;
+
+const Hamburger = ({ isOpen }) => {
   return (
     <StyledHamburger>
-      <Text>Menu</Text>
+      <>
+        <StyledHamburgerTop></StyledHamburgerTop>
+        <StyledHamburgerBottom></StyledHamburgerBottom>
+      </>
     </StyledHamburger>
   );
+};
+
+Hamburger.propTypes = {
+  isOpen: PropTypes.bool,
+};
+
+Hamburger.defaultProps = {
+  isOpen: false,
 };
 
 export default Hamburger;
