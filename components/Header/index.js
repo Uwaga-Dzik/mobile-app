93228a27e425e1826bbfd239ad6776434432b447
Kbuczynski <kamil.buczynski00@interia.pl>
2020-11-28 12:03:55 +0100

    add logo

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Text, View } from "react-native";
 import styled from "styled-components";
 import Menu from "../Menu";
 import Search from "../Search";
@@ -14,12 +13,16 @@ const StyledHeader = styled.View`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
 `;
 
+const StyledLogo = styled.Image`
+  width: 75px;
+  height: 75px;
+  padding: 0;
+`;
+
 const Header = ({ isOpen, setIsOpen }) => {
   return (
     <StyledHeader>
-      <View>
-        <Text>SVG</Text>
-      </View>
+      <StyledLogo source={require("../../assets/logo/logo.png")} />
       <Search />
       <Menu isOpen={isOpen} setIsOpen={setIsOpen} />
     </StyledHeader>
